67befc12aa765ceb40b504faf802e286aceef70b
sebastianrodriguezcorrea <60698491+sebastianrodriguezcorrea@users.noreply.github.com>
2022-08-25 18:49:00 -0500

    Initial commit

---
+++ src/test/java/com/rockola/RockolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rockola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
