f571bf6fc903d7f0af6e3f542226b5cecc1deb80
sebastianrodriguezcorrea <60698491+sebastianrodriguezcorrea@users.noreply.github.com>
2022-09-13 17:56:29 -0500

    Creación de pruebas unitarias para la entidad de cliente

--- src/main/java/com/rockola/servicios/ClienteService.java
+++ src/main/java/com/rockola/servicios/ClienteService.java
@@ -2,29 +2,42 @@ package com.rockola.servicios;
 
 import com.rockola.entidades.Cliente;
 import com.rockola.repositorios.IClienteRepository;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClienteService {
-    
+
     @Autowired
     private IClienteRepository repo;
-    
-    public Cliente crearNuevoCliente(Cliente c){
+
+    public Cliente crearNuevoCliente(Cliente c) {
         Cliente guardado = repo.save(c);
         return guardado;
     }
-    
-    public void actualizarCliente(){
-        
+
+    public Cliente actualizarCliente(Cliente c) {
+        Cliente guardado = repo.save(c);
+        return guardado;
     }
-    
-    public void consultarCliente(){
-        
+
+    public List<Cliente> consultarCliente() {
+        List<Cliente> lista = repo.findAll();
+        return lista;
     }
-    
-    public void eliminarCliente(){
-        
+
+    public Cliente consultarCliente(int documento) {
+        Cliente c = repo.findById(documento).get();
+        return c;
+    }
+
+    public List<Cliente> consultarCliente(String criterio) {
+        List<Cliente> lista = repo.findByNombreContainingOrApellidoContaining(criterio, criterio);
+        return lista;
+    }
+
+    public void eliminarCliente(Cliente c) {
+        repo.delete(c);
     }
 }
