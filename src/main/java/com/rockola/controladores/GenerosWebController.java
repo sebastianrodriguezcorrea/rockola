283d77ea4066cff0e8c0648716d108d14d2a383c
PedroAMendez <111823547+PedroAMendez@users.noreply.github.com>
2022-09-16 18:16:19 -0500

    Creación de listageneros y GenerosWebController

---
+++ src/main/java/com/rockola/controladores/GenerosWebController.java
@@ -0,0 +1,23 @@
+package com.rockola.controladores;
+
+import com.rockola.entidades.GeneroMusical;
+import com.rockola.servicios.GeneroService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GenerosWebController {
+    
+    @Autowired
+    private GeneroService servicio;
+    
+    @GetMapping("/api/generos/")
+    public ResponseEntity<List<GeneroMusical>> consultarGeneros(){
+        List<GeneroMusical> lista = servicio.consultarGeneros();
+        return new ResponseEntity<>(lista, HttpStatus.OK);
+    }
+}
