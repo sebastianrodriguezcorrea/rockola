7d09b19cb6c51530b81d130f83d72451f4def328
PedroAMendez <111823547+PedroAMendez@users.noreply.github.com>
2022-09-16 15:12:12 -0500

    Actualización CancionUnitTests

--- src/test/java/com/rockola/CancionUnitTests.java
+++ src/test/java/com/rockola/CancionUnitTests.java
@@ -8,11 +8,13 @@ import com.rockola.servicios.CancionService;
 import com.rockola.servicios.CompositorService;
 import com.rockola.servicios.GeneroService;
 import com.rockola.servicios.InterpreteService;
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
 
 @SpringBootTest
 public class CancionUnitTests {
@@ -49,16 +51,60 @@ public class CancionUnitTests {
         GeneroMusical g = new GeneroMusical("Vallenato", "vallenato", true);
         GeneroMusical guardado1 = generoService.crearNuevoGenero(g);
         Assertions.assertTrue(guardado1.getId() > 0, "No se pudo crear genero");
-        Interprete inter = new Interprete("Rafael Orozco", "24-03-1954");
+        Interprete inter = new Interprete("Binomio de oro de America", "1976");
         Interprete guardado2 = interpreteService.crearNuevoInterprete(inter);
         Assertions.assertTrue(guardado2.getId() > 0, "No se pudo crear interprete");
         Compositor comp = new Compositor("Ivan Ovalle", "16-01-1963");
         Compositor guardado3 = compositorService.crearNuevoCompositor(comp);
         Assertions.assertTrue(guardado3.getId() > 0, "No se pudo crear compositor");
-        Cancion c = new Cancion("La muralla verde", 1986, "2:40", g, inter, comp);
+        Cancion c = new Cancion("El amor es mas grande que yo", 1989, "4:20", g, inter, comp);
         cancionService.guardarCancion(c);
         Assertions.assertDoesNotThrow(() -> {
             cancionService.guardarCancion(c);
         }, "Error al guardar la cancion");
+    }
+    
+    @Test
+    @Disabled
+    void verificarSiNoSeGuardaCancionInvalida() {
+        Cancion c = new Cancion(null, 4, null, null, null, null);
+        Assertions.assertThrows(DataIntegrityViolationException.class, () -> {
+            cancionService.guardarCancion(c);
+        }, "No se generó excepción desde la base de datos.");
+    }
+    
+    @Test
+    @Disabled
+    void verificarSiSeCarganTodasLasCanciones() {
+        List<Cancion> lista = cancionService.cargarCanciones();
+        Assertions.assertTrue(lista.size() > 0, "No se cargaron las canciones");
+    }
+    
+    @Test
+    @Disabled
+    void verificarSiSeCargaUnaCancionExistentePorId() {
+        Cancion c = cancionService.cargarCancion(1);
+        Assertions.assertNotNull(c, "No se cargó la canción");
+    }
+    
+//    @Test
+//    void verificarSiSeCargaUnaCancionExistentePorCriterio() {
+//        List<Cancion> lista = cancionService.cargarCancionesPorCriterio("la muralla verde");
+//        Assertions.assertTrue(lista.size() > 0, "No se cargaron las canciones existentes");
+//    }
+    
+    @Test
+    @Disabled
+    void verificarSiNoSeCargaUnaCancionNoExistentePorId() {
+        Cancion c = cancionService.cargarCancion(10000000);
+        Assertions.assertNull(c, "Se cargó género inexistente");
+    }
+    
+    @Test
+    @Disabled
+    void verificarSiSeEliminaUnaCancionExistente() {
+        cancionService.eliminarCancion(2);
+        Cancion eliminado = cancionService.cargarCancion(2);
+        Assertions.assertNull(eliminado, "No se eliminó el interprete existente");
     }
 }
