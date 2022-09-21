d3c0900e35df3f56ca268f18b52fdcffc972d686
sebastianrodriguezcorrea <60698491+sebastianrodriguezcorrea@users.noreply.github.com>
2022-09-20 21:31:22 -0500

    Cambios realizados al frontend y al backend del proyecto

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # rockola
- 
+
+Proyecto LaRockola.com®
+
+Este es un proyecto educativo creado por estudiantes del programa de Desarrollo de Software del Ministerio de Tecnologías de la Información y Comunicaciones (MinTic) y la Universidad Industrial de Santander  (UIS). 
+Es una plataforma musical  diseñada para que los usuarios tengan acceso a un gran repositorio musical, donde podrán hacer una búsqueda por nombre de canción, nombre de artista o género musical y disfrutar horas y horas de las canciones de tus artistas favoritos. Adicionalmente cuenta con la opción de crear las playlist que quieras con tus mejores canciones, para ello sólo necesitas crear una cuenta totalmente gratis, ingresando tu nombre, correo electrónico, nombre de usuario y contraseña. 
+No necesitas instalación, ya que LaRockola.com®  está diseñada para que puedas acceder desde cualquier navegador y cualquier dispositivo cuando quieras.
+
--- src/main/java/com/rockola/controladores/CuentaController.java
+++ src/main/java/com/rockola/controladores/CuentaController.java
@@ -1,8 +1,15 @@
 package com.rockola.controladores;
 
+import com.rockola.entidades.Cuenta;
+import com.rockola.servicios.CuentaService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  * index.html
@@ -11,8 +18,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CuentaController {
 
+    @Autowired
+    private CuentaService cuentaService;
+
     @GetMapping("/")
-    public String cargarIndex() {
+    public String cargarIndex(Model model) {
+        List<Cuenta> cuentas = cuentaService.consultarCuenta();
+        model.addAttribute("cuentas", cuentas);
         return "index";
     }
 
@@ -24,5 +36,24 @@ public class CuentaController {
     @GetMapping("/formcuentas")
     public String cargarFormCuentas() {
         return "formcuentas";
+    }
+
+    @PostMapping("/inicio")
+    public String cargarFormInicio(Model model, @RequestParam(name = "criterio", required = false) String criterio,
+            @RequestParam(name = "criterio2", required = false) String criterio2, RedirectAttributes redirectAttributes) {
+        List<Cuenta> cuentas = cuentaService.consultarCuenta();
+        model.addAttribute("cuentas", cuentas);
+        String res = "";
+        for (Cuenta cuenta : cuentas) {
+            if (cuenta.getCorreo().equals(criterio) && cuenta.getPassword().equals(criterio2)) {
+                res = "inicio";
+                redirectAttributes.addFlashAttribute("successMessage", "Se ha iniciado sesión");
+                break;
+            } else {
+                redirectAttributes.addFlashAttribute("errorMessage", "Credenciales no validas");
+                res = "index";
+            }
+        }
+        return res;
     }
 }
